fe99b3d478bbd66142e7f8bb9f835c708d43e4e3
fuling <c1694687567@gmail.com>
2024-02-08 21:42:39 +0800

    0

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -15,7 +15,7 @@ export async function onRequest(context: {
 
   if (
     cookie.includes(cookieKeyValue) ||
-    CFP_ALLOWED_PATHS.includes(pathname) ||
+    !CFP_PROTECTED_PATHS.includes(pathname) ||
     !env.CFP_PASSWORD
   ) {
     // Correct hash in cookie, allowed path, or no password set.
